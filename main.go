2de065533052ecddb9fd725bd2e5c7e092a9dc2c
wobusbz <1135455278@qq.com>
2020-04-14 00:43:25 +0800

    全局初始化auth

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	"despair/app/index/controller"
+	"despair/app/middleware"
 	"despair/app/routers"
 	"despair/common/logger"
-	"despair/conf"
 	"despair/db"
 	"github.com/gin-gonic/gin"
 )
 
 func init() {
-	conf.InitConfig()
 	logger.InitLogger()
 	db.InitMysql("mysql", "root:@tcp(127.0.0.1:3306)/wuhuarou?charset=utf8&parseTime=True&loc=Local")
 	logger.Debug("mysql connect succ")
@@ -17,13 +17,18 @@ func init() {
 
 func main() {
 
-	r := gin.Default()
+	r := gin.New()
 	// 中间件
+	// 初始化基础模型，激活auth认证
+	// 全局初始化一个auth
+	baseController := controller.NewBaseController()
+	r.Use(middleware.AuthMiddleware(baseController.Auth))
 
 	// 注册前台路由
 	indexRouter := r.Group("")
 	routers.ResistIndexRoutes(indexRouter)
 	logger.Debug("abc%s", "abc")
+
 	// 注册后端路由
 	adminRouter := r.Group("/admin")
 	routers.RegisterAdminRouter(adminRouter)
